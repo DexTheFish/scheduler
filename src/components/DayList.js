1d0e7eb844f9a082c1c78fd58eff834ab04d65d3
Declan Davis <dexthefish@gmail.com>
2022-07-13 17:28:35 -0400

    add state to Application

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,12 +3,10 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 
-  const { days } = props;
-  const day = props.day;
-  const setDay = props.setDay;
+  const { days, day, setDay } = props;
 
   const DayListItemArray = days.map((dayObj) => {
-    return <DayListItem key={dayObj.id} {...dayObj} selected={dayObj.name === day} />
+    return <DayListItem key={dayObj.id} {...dayObj} setDay={setDay} selected={dayObj.name === day} />
   })
 
   return (
