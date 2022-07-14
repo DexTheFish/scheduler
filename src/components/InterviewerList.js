e6ae06a59a125e4e80c554184debcd5fc732e8c0
Declan Davis <dexthefish@gmail.com>
2022-07-14 16:49:36 -0400

    refactor components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,20 +5,19 @@ import InterviewerListItem from "./InterviewerListItem";
 // Refactor the hardcoded content to use props & state
 
 export default function InterviewerList(props) {
-  const { interviewers, interviewer, setInterviewer} = props;
-  const InterviewerListItemArray = interviewers.map((interviewerObj) => {
+  const { interviewer, setInterviewer} = props;
+  const interviewers = props.interviewers.map((interviewerObj) => {
     return <InterviewerListItem 
-    key={interviewerObj.id}
-    setInterviewer={setInterviewer}
-    selected={interviewer === interviewerObj.id}
-    {...interviewerObj}  />
+      key={interviewerObj.id}
+      setInterviewer={() => setInterviewer(interviewerObj.id)}
+      selected={interviewer === interviewerObj.id}
+      {...interviewerObj}
+      />
   })
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer List</h4>
-      <ul className="interviewers__list">{InterviewerListItemArray}</ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   )
-
-
 }
