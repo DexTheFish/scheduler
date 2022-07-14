096f7308af5a58054f6b8d77dfc753db4a9cb42b
Declan Davis <dexthefish@gmail.com>
2022-07-14 17:05:03 -0400

    rename props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,18 +2,22 @@ import React from "react";
 import "components/InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem";
 
-// Refactor the hardcoded content to use props & state
 
 export default function InterviewerList(props) {
-  const { interviewer, setInterviewer} = props;
+
+  const value = props.interviewer;
+  const onChange = props.setInterviewer;
   const interviewers = props.interviewers.map((interviewerObj) => {
-    return <InterviewerListItem 
-      key={interviewerObj.id}
-      setInterviewer={() => setInterviewer(interviewerObj.id)}
-      selected={interviewer === interviewerObj.id}
-      {...interviewerObj}
+    return (
+      <InterviewerListItem 
+        key={interviewerObj.id}
+        setInterviewer={() => onChange(interviewerObj.id)}
+        selected={value === interviewerObj.id}
+        {...interviewerObj}
       />
-  })
+    )
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer List</h4>
--- stories/index.js
+++ stories/index.js
@@ -142,6 +142,6 @@ storiesOf("InterviewerList", module)
   .add("Clickable", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      onChange={action("setInterviewer")}
     />
   ));
