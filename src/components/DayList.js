a3c08e8b86b31db4a4031dad60b9d36994416af2
Declan Davis <dexthefish@gmail.com>
2022-07-14 16:57:35 -0400

    refactor by renaming some props

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,15 +3,18 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 
-  const { days, day, setDay } = props;
+  const { days, value, onChange } = props;
 
   const DayListItemArray = days.map((dayObj) => {
-    return <DayListItem
-    key={dayObj.id}
-    {...dayObj}
-    setDay={setDay}
-    selected={dayObj.name === day} />
-  })
+    return (
+      <DayListItem
+        key={dayObj.id}
+        {...dayObj}
+        setDay={onChange}
+        selected={dayObj.name === value}
+      />
+    );
+  });
 
   return (
     <ul>
