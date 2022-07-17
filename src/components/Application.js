bb2e05b29a404c239cd820e927072a567b9ea4a9
Declan Davis <dexthefish@gmail.com>
2022-07-16 23:06:45 -0400

    grouping together promises

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,61 +5,28 @@ import Appointment from "./Appointment";
 import "components/Application";
 import axios from "axios";
 
-// Mock Data
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
 
 export default function Application(props) {
 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    // appointments: {}
+    appointments: {}
   });
   const setDay = (day) => setState({ ...state, day });
-  const setDays = (days) => setState(prev => ({ ...prev, days }));
+//  const setDays = (days) => setState(prev => ({ ...prev, days }));
+  const dailyAppointments = [];
+
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days")
-    .then((res) => setDays(res.data));
-  }, [])
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments")
+    ]).then((all) => {
+      console.log(all[0]);
+      console.log(all[1]);
+    })
+  })
 
 
   return (
@@ -85,7 +52,7 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map(appointment => {
+        {Object.values(dailyAppointments).map(appointment => {
           return <Appointment key={appointment.id} {...appointment}/>
         })}
         <Appointment key="last" time="5pm" id={100} />
