9d853ee8923c3f7eea6bdfeeac788448cc870597
Declan Davis <dexthefish@gmail.com>
2022-07-16 23:16:35 -0400

    group axios requests together

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "components/Application";
 import axios from "axios";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -23,10 +24,10 @@ export default function Application(props) {
       axios.get("http://localhost:8001/api/days"),
       axios.get("http://localhost:8001/api/appointments")
     ]).then((all) => {
-      console.log(all[0]);
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
       console.log(all[1]);
     })
-  })
+  }, [])
 
 
   return (
