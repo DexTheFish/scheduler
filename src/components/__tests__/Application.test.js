64e383df069c92c4e9b748634e534e25c9e684d5
Declan Davis <dexthefish@gmail.com>
2022-07-20 20:57:48 -0400

    add to App tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, getByAltText, getByPlaceholderText, getAllByTestId, prettyDOM } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, queryByText, queryByAltText, getByAltText, getByPlaceholderText, getAllByTestId, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -47,4 +47,32 @@ describe("Application", () => {
 
   });
 
+  // the following two tests are unfinished. see activity "The Next Tests" 
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+  
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    // 3. Click the "Delete" button on the booked appointment.
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+  
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+  
+    // 4. Check that the confirmation message is shown.
+    // 5. Click the "Confirm" button on the confirmation.
+    // 6. Check that the element with the text "Deleting" is displayed.
+    // 7. Wait until the element with the "Add" button is displayed.
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+  
+    debug();
+  });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", () => {
+    // insert test here...
+  });
 })
