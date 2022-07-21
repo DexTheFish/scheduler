08321bbd1e37a2d4659a9af6fd50b29f41682f96
Declan Davis <dexthefish@gmail.com>
2022-07-20 20:29:52 -0400

    finish integration tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -22,22 +22,11 @@ describe("Application", () => {
   });
   
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    // Render the Application.
-    // Wait until the text "Archie Cohen" is displayed.
-    // Click the "Add" button on the first empty appointment.
-    // Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
-    // Click the first interviewer in the list.
-    // Click the "Save" button on that same appointment.
-    // Check that the element with the text "Saving" is displayed.
-    // Wait until the element with the text "Lydia Miller-Jones" is displayed.
-    // Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
-    const { container } = render(<Application />);
+
+    const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"))
     const appointments = getAllByTestId(container, "appointment");
-    // console.log(appointments);
-    // console.log(prettyDOM(appointments));
-    // console.log(prettyDOM(appointments[0]));
     fireEvent.click(getByAltText(appointments[0], "Add"));
 
     fireEvent.change(getByPlaceholderText(appointments[0], /enter student name/i), {
@@ -46,9 +35,16 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointments[0], "Sylvia Palmer"));
 
     fireEvent.click(getByText(appointments[0], "Save"));
+    expect(getByText(appointments[0], "saving...")).toBeInTheDocument();
+    
+    await waitForElement(() => getByText(appointments[0], "Lydia Miller-Jones"));
 
-    // If we see act() warnings, it is because we are causing the component to update when we click the "Save" button. We need to allow the component to finish the operation and then verify that the expected behaviour occurs. We can continue to ignore these warnings while we build this test as long as it continues to pass.
+    const day = getAllByTestId(container, "day").find(day =>
+      getByText(day, "Monday")
+    );
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
   });
-
 
 })
